c9b91aea7f91336881438c2d445caee9899d0020
TeraSurror <harshshelar22@gmail.com>
2025-04-15 20:09:31 -0400

    added cards: needs to be fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,12 @@
 import { H1 } from "@/components/typography/typography";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default function Home() {
@@ -16,12 +24,97 @@ export default function Home() {
           <TabsTrigger value="contact">Contact</TabsTrigger>
         </TabsList>
         <br />
+        <hr />
+        <br />
         <TabsContent value="about">About view</TabsContent>
         <TabsContent value="resume">Resume View</TabsContent>
-        <TabsContent value="projects">Projects View</TabsContent>
+        <TabsContent value="projects">
+          <Projects />
+        </TabsContent>
         <TabsContent value="contact">Contact View</TabsContent>
       </Tabs>
       <hr />
+    </div>
+  );
+}
+
+function Projects() {
+  const projectList = [
+    {
+      title: "Project title",
+      description: "Some description about the project",
+      link: "some link man idk",
+    },
+    {
+      title: "Project title",
+      description: "Some description about the project",
+      link: "some link man idk",
+    },
+    {
+      title: "Project title",
+      description: "Some description about the project",
+      link: "some link man idk",
+    },
+    {
+      title: "Project title",
+      description: "Some description about the project",
+      link: "some link man idk",
+    },
+    {
+      title: "Project title",
+      description: "Some description about the project",
+      link: "some link man idk",
+    },
+    {
+      title: "Project title",
+      description: "Some description about the project",
+      link: "some link man idk",
+    },
+    {
+      title: "Project title",
+      description: "Some description about the project",
+      link: "some link man idk",
+    },
+    {
+      title: "Project title",
+      description: "Some description about the project",
+      link: "some link man idk",
+    },
+    {
+      title: "Project title",
+      description: "Some description about the project",
+      link: "some link man idk",
+    },
+    {
+      title: "Project title",
+      description: "Some description about the project",
+      link: "some link man idk",
+    },
+    {
+      title: "Project title",
+      description: "Some description about the project",
+      link: "some link man idk",
+    },
+  ];
+
+  return (
+    <div className="flex flex-wrap justify-between">
+      {projectList.map((project, idx) => {
+        return (
+          <Card key={idx}>
+            <CardHeader>
+              <CardTitle>{project.title}</CardTitle>
+              <CardDescription>{project.description}</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>Some Project stuff</p>
+            </CardContent>
+            <CardFooter>
+              <p>feet hehehe</p>
+            </CardFooter>
+          </Card>
+        );
+      })}
     </div>
   );
 }
